bc8cca77c8f344e57b4e7d8afd48ee30e1b85038
MananAmabaliya <mananambaliya1704@gmail.com>
2026-07-08 20:44:41 +0530

    Time: 0 ms (100%), Space: 20.2 MB (97.5%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.c
@@ -0,0 +1,24 @@
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
+    int m = matrixSize;
+    int n = matrixColSize[0];
+
+    int row[m];
+    int col[n];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (matrix[i][j] == 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (row[i] == 1 || col[j] == 1) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
