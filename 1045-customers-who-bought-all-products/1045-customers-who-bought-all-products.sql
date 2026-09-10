8f1389ba79ed7bd04863d55b2aeb3b083deef44d
MananAmbaliya <mananambaliya1704@gmail.com>
2026-09-10 10:43:50 +0530

    Time: 1550 ms (23.76%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select customer_id
+from Customer c
+left join Product p
+on c.product_key = p.product_key 
+group by customer_id
+having count(distinct c.product_key) = (select count(product_key) from Product)
